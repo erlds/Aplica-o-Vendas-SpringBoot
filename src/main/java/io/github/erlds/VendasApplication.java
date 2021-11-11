f505c3278cb1d19c9706b943b3206c6be9bf076e
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-11 20:58:07 -0300

    Add file myconfiguration

--- src/main/java/io/github/erlds/VendasApplication.java
+++ src/main/java/io/github/erlds/VendasApplication.java
@@ -1,6 +1,8 @@
 package io.github.erlds;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
     @GetMapping("/hello")
     public String helloWorld(){
-        return "hello world";
+        return applicationName;
     }
 
     public static void main(String[] args) {
