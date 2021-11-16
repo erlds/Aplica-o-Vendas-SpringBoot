7d467b14eaa28ae6416e37d49b9f40f4495add72
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-16 19:58:27 -0300

    Saving and recovering clients

--- src/main/java/io/github/erlds/VendasApplication.java
+++ src/main/java/io/github/erlds/VendasApplication.java
@@ -1,11 +1,29 @@
 package io.github.erlds;
 
 
+import io.github.erlds.domain.entity.Cliente;
+import io.github.erlds.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Evaristo"));
+            clientes.salvar(new Cliente("Irenice"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class,args);
