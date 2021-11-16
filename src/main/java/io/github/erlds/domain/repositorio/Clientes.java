ae2910f5e5b45183aeb2a5a07936521c30a5c45f
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-16 20:54:50 -0300

    Concluding client registration

--- src/main/java/io/github/erlds/domain/repositorio/Clientes.java
+++ src/main/java/io/github/erlds/domain/repositorio/Clientes.java
@@ -16,6 +16,8 @@ public class Clientes {
 
     private static String INSERT = "INSERT INTO CLIENTE (nome) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static String UPDATE = "UPDATE CLIENTE SET NOME = ? WHERE ID = ? ";
+    private static String DELETE = "DELETE FROM CLIENTE WHERE ID = ?";
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -25,15 +27,41 @@ public class Clientes {
         return cliente;
     }
 
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()
+        });
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" WHERE NOME LIKE ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
     public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
                 Integer id = rs.getInt("id");
                 String nome = rs.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 
 }
