f7fcec02f8fab4e9d3f983f5f712f646d6aca042
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-16 21:17:06 -0300

    Mapping the Entity Cliente to JPA

--- src/main/java/io/github/erlds/domain/entity/Cliente.java
+++ src/main/java/io/github/erlds/domain/entity/Cliente.java
@@ -1,8 +1,21 @@
 package io.github.erlds.domain.entity;
 
+import javax.persistence.*;
+
+// A anotation @Table seria necessária caso fosse usado uma tabela com nome diferente da unidade
+
+// Sinaliza essa classe como uma entidade (tabela)
+@Entity
 public class Cliente {
 
+    //Define a primary key da entidade
+    @Id
+    // Gera a estratégia usada para geração dos IDs
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    // A annotation @Column(name = "ID") seria necessária caso id tivesse um nome diferente na tabela
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente() {
