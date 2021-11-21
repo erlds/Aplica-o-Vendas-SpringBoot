359bb9109fa9dcd85430de2b0df0828d70e8a75b
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-21 10:42:37 -0300

    Add post requisition in ClienteController

--- src/main/java/io/github/erlds/domain/entity/Cliente.java
+++ src/main/java/io/github/erlds/domain/entity/Cliente.java
@@ -1,5 +1,7 @@
 package io.github.erlds.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -19,6 +21,7 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @JsonIgnore
     // Atributo necessário para o mapeamento
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
--- src/main/java/io/github/erlds/rest/controller/ClienteController.java
+++ src/main/java/io/github/erlds/rest/controller/ClienteController.java
@@ -32,6 +32,13 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save ( @RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
 
 
 
