338831c9e696413f392a03ea8228730ecc6a2256
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-27 13:17:39 -0300

    Add Classes ApplicationControllerAdvice and ApiErrors

---
+++ src/main/java/io/github/erlds/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package io.github.erlds.rest.controller;
+
+import io.github.erlds.exception.RegraNegocioException;
+import io.github.erlds.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+}
