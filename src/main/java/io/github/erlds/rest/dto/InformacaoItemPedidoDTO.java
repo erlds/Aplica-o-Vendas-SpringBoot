deb8bdceeb3c65ffb7371b3da84da361b0fb9887
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-28 10:12:11 -0300

    Correct GET pedidos error

--- src/main/java/io/github/erlds/rest/dto/InformacaoItemPedidoDTO.java
+++ src/main/java/io/github/erlds/rest/dto/InformacaoItemPedidoDTO.java
@@ -1,9 +1,15 @@
 package io.github.erlds.rest.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class InformacaoItemPedidoDTO {
     private String descricaoProduto;
