c188bd933ea37f9d446dff95dba3bb1ca481366b
Evaristo Ramalho <evaristo.ra@hotmail.com>
2021-11-30 10:05:33 -0300

    Decode token

--- src/main/java/io/github/erlds/JwtService.java
+++ src/main/java/io/github/erlds/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.erlds;
 
 import io.github.erlds.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -25,6 +28,8 @@ public class JwtService {
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
         Instant instant= dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
+
+
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
@@ -32,4 +37,30 @@ public class JwtService {
                 .signWith(SignatureAlgorithm.HS512,chaveAssinatura)
                 .compact();
     }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token){
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant().
+                            atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return LocalDateTime.now().isAfter(data);
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return obterClaims(token).getSubject();
+    }
+
 }
